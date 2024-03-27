8daf27b1c11236e926b67dc691e2a9f475309fe5
sw.kim <sw.kim@fusionsoft.co.kr>
2024-03-27 15:21:42 +0900

    오후 3:21 2024-03-27

--- src/main/java/hellojpa/JpaMain2.java
+++ src/main/java/hellojpa/JpaMain2.java
@@ -4,6 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.time.LocalDateTime;
 import java.util.List;
 
 public class JpaMain2 {
@@ -23,6 +24,8 @@ public class JpaMain2 {
             Member2 member = new Member2();
             member.setUsername("member1");
             member.changeTeam(team);
+            member.setCreatedBy("kim");
+            member.setCreatedDate(LocalDateTime.now());
 
             em.persist(member);
 
